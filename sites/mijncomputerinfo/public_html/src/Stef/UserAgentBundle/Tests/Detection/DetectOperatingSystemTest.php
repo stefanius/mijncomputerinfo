5fc61d176339ea138dd9105bd0c3a7219936dbe6
stefanius <KIT_kat_16>
2015-08-26 22:44:29 +0200

    Fixed Android detection

--- sites/mijncomputerinfo/public_html/src/Stef/UserAgentBundle/Tests/Detection/DetectOperatingSystemTest.php
+++ sites/mijncomputerinfo/public_html/src/Stef/UserAgentBundle/Tests/Detection/DetectOperatingSystemTest.php
@@ -49,6 +49,7 @@ class DetectOperatingSystemTest extends \PHPUnit_Framework_TestCase
             ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36", "Mac OS X"],
             ["Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36", "Windows XP"],
             ["Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36", "Windows XP"],
+            ["Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30", "Android"],
             //IE
             ["Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko", "Windows 8.1"],
             ["Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0", "Windows 7"],
